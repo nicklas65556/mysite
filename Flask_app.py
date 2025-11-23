7b25828c7f9727a70b98a888c49c7f7c342ba485
Nicklas Koch <nicklas65556@gmail.com>
2025-11-23 14:45:05 -0500

    second commit

--- Flask_app.py
+++ Flask_app.py
@@ -1,1 +1,11 @@
-from flask import flask, render_template
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    return render_template('index.html')
+ 
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
